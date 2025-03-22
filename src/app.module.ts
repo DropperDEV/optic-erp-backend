2abed9b557fabff5407b486c3b64bb18bec3e3af
JAMES_COSTA <jamescosta4747@gmail.com>
2025-03-21 23:30:49 -0300

    :zap: approving dotenv use

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomersModule } from './customers/customers.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CustomersModule],
+  imports: [CustomersModule, AuthModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
